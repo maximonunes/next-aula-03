3df2394c85d9c53e96dfd8f1b417c4fdc1418da8
maximonunes <a22408741@alunos.ulht.pt>
2025-11-25 13:00:29 +0000

    count

--- components/Contador/page.tsx
+++ components/Contador/page.tsx
@@ -13,7 +13,7 @@ export default function Contador() {
         <section className="bg-blue-300 p-2 pb-4 mt-6 rounded-xl">
 
             <h2>Contador</h2>
-            <p>Contador vai em <span>   </span>!</p>
+            <p>Contador vai em <span>{count}</span>!</p>
 
             <button
                 className="bg-green-500 hover:bg-green-600 active:bg-green-700 hover:pointer text-white font-bold py-2 px-4 mx-2 border border-green-700 rounded"
